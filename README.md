eb827cd8a6ba377cf1d142976ca7e1f8b0954c78
mdeller-ping <mdeller@pingidentity.com>
2020-08-20 16:36:19 -0400

    initial version

---
+++ README.md
@@ -0,0 +1,8 @@
+# pingone-for-customers-slack-webhook
+
+Simple illustration of receiving a PingOne for Customers Webhook and sending it to a Slack Channel
+
+Update index.php with the URL of your Slack Webhook
+
+Create a Webhook in your PingOne admin console with the URL of where you host index.php
+
---
+++ dist/index.php
@@ -0,0 +1,37 @@
+<?php
+
+  // set content type to json as we pretend to be a REST API
+
+  header('Content-Type: application/json');
+  include ("httpful.phar");
+
+  // receive payload from caller
+
+  $method = $_SERVER['REQUEST_METHOD'];
+  $request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
+  $input = file_get_contents('php://input');
+  $response = json_decode($input);
+  $base64 = base64_encode( $input );
+
+  // my chance to see the payload
+
+  $payload = $response[0]->result->description;
+
+  // call slack hook
+
+  $uri = 'https://hooks.slack.com/services/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';
+
+  $data = json_encode(array('text' => $payload));
+
+  // // // echo $data;
+
+  // // // echo json_decode($data);
+
+  $response = \Httpful\Request::post($uri)
+    ->sendsJson()
+    ->body($data)
+    ->send();
+
+  // echo json_decode($response);
+
+?>
